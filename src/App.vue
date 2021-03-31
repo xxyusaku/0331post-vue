1bdb690b5e76873bcf905f956ce713db8d746196
xxyusaku <premiumhome0yy@gmail.com>
2021-03-31 12:25:26 +0900

    1

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="text">
+    <button @click="getAddress">住所自動入力</button>
+    <p>Address :{{ }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  props: [""],
+  data() {
+    return {
+    };
+  },
+  async created() {
+    const getAddress = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=inLiNPQOtiWwVg8PhMuys0iOfWuxmIG4nroFizN`
+    );
+  }
+};
+</script>
